120b6291ad043c10f9b0d79a557e71350f45f0b5
Joseph C. Osborn <joseph.osborn@pomona.edu>
2023-02-09 11:17:29 -0800

    Fix typo in dist-update

--- dist-update.sh
+++ dist-update.sh
@@ -2,5 +2,5 @@
 
 rm -rf retroarch-web
 curl -o /tmp/retro.7z https://buildbot.libretro.com/nightly/emscripten/RetroArch.7z
-7z x /tmp/retro.7z -o .
+7z x /tmp/retro.7z -o.
 mv retroarch retroarch-web
