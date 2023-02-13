a80463f54bff6e5f5a3dbc5dd6760df26294441d
Joseph C. Osborn <joseph.osborn@pomona.edu>
2023-02-13 11:49:48 -0800

    add download caching to fetchfs. all done with that for now.

--- frontend-web/src/main.ts
+++ frontend-web/src/main.ts
@@ -30,7 +30,7 @@ retro_args.push("/home/web_user/content/" + content);
 
 loadRetroArch(core,
   function () {
-    let p1 = fetch.registerFetchFS(("assets/frontend/bundle/.index-xhr"), "assets/frontend/bundle", "/home/web_user/retroarch/bundle");
+    let p1 = fetch.registerFetchFS(("assets/frontend/bundle/.index-xhr"), "assets/frontend/bundle", "/home/web_user/retroarch/bundle", true);
     let xfs_content_files: fetch.Index = { "retroarch.cfg": null };
     xfs_content_files[content] = null;
     if (entryState) {
@@ -39,7 +39,7 @@ loadRetroArch(core,
     if (movie) {
       xfs_content_files["movie.bsv"] = null;
     }
-    let p2 = fetch.registerFetchFS(xfs_content_files, content_folder, "/home/web_user/content");
+    let p2 = fetch.registerFetchFS(xfs_content_files, content_folder, "/home/web_user/content", false);
     Promise.all([p1, p2]).then(function () {
       if (entryState) {
         fetch.mkdirp("/home/web_user/retroarch/userdata/states");
