c5794918d0a38de732d305a64159537af3e28577
Joseph C. Osborn <joseph.osborn@pomona.edu>
2023-02-13 11:09:52 -0800

    whitespace

--- frontend-web/src/fetchfs.ts
+++ frontend-web/src/fetchfs.ts
@@ -2,59 +2,59 @@ type FileContents = null|Index;
 export interface Index extends Record<string, FileContents> {}
 
 function min(n:number,m:number) : number {
-    if (n <= m) { return n; }
-    return m;
+  if (n <= m) { return n; }
+  return m;
 }
 
 export async function registerFetchFS(index:string | Index, root:string, mount:string) {
-    let index_file_tree;
-    if (typeof index === "string") {
-        const index_result = await fetch(index);
-        index_file_tree = await index_result.json();
-    } else {
-        index_file_tree = index;
-    }
-    // TODO: keep our own cache so that we don't need to make 5000 requests.
-    // OR, download and unzip a bundle of assets.
-    // OR, just remove overlays and unnecessary GUI icons and stuff from the bundles
-    let files:[string,string][] = [];
-    mkdirp(mount);
-    fetchDirectory(index_file_tree, root, mount, files);
-    const batch_size = 100;
-    for(let i = 0; i < files.length; i+=batch_size) {
-        let file_batch = files.slice(i,min(i+batch_size,files.length));
-        await Promise.all(file_batch.map(([from,to]) => fetchFile(from,to)));
-    }
+  let index_file_tree;
+  if (typeof index === "string") {
+    const index_result = await fetch(index);
+    index_file_tree = await index_result.json();
+  } else {
+    index_file_tree = index;
+  }
+  // TODO: keep our own cache so that we don't need to make 5000 requests.
+  // OR, download and unzip a bundle of assets.
+  // OR, just remove overlays and unnecessary GUI icons and stuff from the bundles
+  let files:[string,string][] = [];
+  mkdirp(mount);
+  fetchDirectory(index_file_tree, root, mount, files);
+  const batch_size = 100;
+  for(let i = 0; i < files.length; i+=batch_size) {
+    let file_batch = files.slice(i,min(i+batch_size,files.length));
+    await Promise.all(file_batch.map(([from,to]) => fetchFile(from,to)));
+  }
 }
 
 function fetchDirectory(index_file_tree:Index, root:string, mount:string, files:[string,string][]) {
-    for (let file of Object.keys(index_file_tree)) {
-        const contents = index_file_tree[file];
-        if (contents === null) {
-            files.push([root+"/"+file, mount+"/"+file]);
-        } else {
-            mkdir(mount+"/"+file);
-            fetchDirectory(contents, root+"/"+file, mount+"/"+file, files);
-        }
+  for (let file of Object.keys(index_file_tree)) {
+    const contents = index_file_tree[file];
+    if (contents === null) {
+      files.push([root+"/"+file, mount+"/"+file]);
+    } else {
+      mkdir(mount+"/"+file);
+      fetchDirectory(contents, root+"/"+file, mount+"/"+file, files);
     }
+  }
 }
 
 function fetchFile(from:string, to:string):Promise<void> {
-    return fetch(from).then(r => r.arrayBuffer()).then(b => FS.writeFile(to, new Uint8Array(b)));
+  return fetch(from).then(r => r.arrayBuffer()).then(b => FS.writeFile(to, new Uint8Array(b)));
 }
 
 export function mkdir(path:string) {
-    try {
-        FS.mkdir(path);
-    } catch {
-        //ignore
-    }
+  try {
+    FS.mkdir(path);
+  } catch {
+    //ignore
+  }
 }
 
 export function mkdirp(path:string) {
-    let sofar = [];
-    for (let chunk of path.split("/")) {
-        sofar.push(chunk);
-        mkdir(sofar.join("/"));
-    }
+  let sofar = [];
+  for (let chunk of path.split("/")) {
+    sofar.push(chunk);
+    mkdir(sofar.join("/"));
+  }
 }
--- frontend-web/src/main.ts
+++ frontend-web/src/main.ts
@@ -15,81 +15,81 @@ let content_base = content.substring(0, content.lastIndexOf("."));
 
 let retro_args = ["-v"];
 if (entryState) {
-    retro_args.push("-e");
-    retro_args.push("1");
+  retro_args.push("-e");
+  retro_args.push("1");
 }
 if (movie) {
-    retro_args.push("-P");
-    retro_args.push("/home/web_user/content/movie.bsv");
+  retro_args.push("-P");
+  retro_args.push("/home/web_user/content/movie.bsv");
 } else {
-    retro_args.push("-R");
-    retro_args.push("/home/web_user/retroarch/userdata/movie.bsv");
+  retro_args.push("-R");
+  retro_args.push("/home/web_user/retroarch/userdata/movie.bsv");
 }
 retro_args.push("/home/web_user/content/" + content);
 
 
 loadRetroArch(core,
-    function () {
-        let p1 = fetch.registerFetchFS(("assets/frontend/bundle/.index-xhr"), "assets/frontend/bundle", "/home/web_user/retroarch/bundle");
-        let xfs_content_files: fetch.Index = { "retroarch.cfg": null };
-        xfs_content_files[content] = null;
-        if (entryState) {
-            xfs_content_files["entry_state"] = null;
-        }
-        if (movie) {
-            xfs_content_files["movie.bsv"] = null;
-        }
-        let p2 = fetch.registerFetchFS(xfs_content_files, content_folder, "/home/web_user/content");
-        Promise.all([p1, p2]).then(function () {
-            if (entryState) {
-                fetch.mkdirp("/home/web_user/retroarch/userdata/states");
-                copyFile("/home/web_user/content/entry_state",
-                    "/home/web_user/retroarch/userdata/states/" + content_base + ".state1.entry");
-            }
-            copyFile("/home/web_user/content/retroarch.cfg", "/home/web_user/retroarch/userdata/retroarch.cfg");
-            retroReady();
-        });
+  function () {
+    let p1 = fetch.registerFetchFS(("assets/frontend/bundle/.index-xhr"), "assets/frontend/bundle", "/home/web_user/retroarch/bundle");
+    let xfs_content_files: fetch.Index = { "retroarch.cfg": null };
+    xfs_content_files[content] = null;
+    if (entryState) {
+      xfs_content_files["entry_state"] = null;
+    }
+    if (movie) {
+      xfs_content_files["movie.bsv"] = null;
+    }
+    let p2 = fetch.registerFetchFS(xfs_content_files, content_folder, "/home/web_user/content");
+    Promise.all([p1, p2]).then(function () {
+      if (entryState) {
+        fetch.mkdirp("/home/web_user/retroarch/userdata/states");
+        copyFile("/home/web_user/content/entry_state",
+          "/home/web_user/retroarch/userdata/states/" + content_base + ".state1.entry");
+      }
+      copyFile("/home/web_user/content/retroarch.cfg", "/home/web_user/retroarch/userdata/retroarch.cfg");
+      retroReady();
     });
+  });
 
 function copyFile(from: string, to: string): void {
-    let buf = FS.readFile(from);
-    FS.writeFile(to, buf);
+  let buf = FS.readFile(from);
+  FS.writeFile(to, buf);
 }
 
 function retroReady(): void {
-    let prev = document.getElementById("webplayer-preview")!;
-    prev.classList.add("loaded");
-    prev.addEventListener(
-        "click",
-        function () {
-            let canv = <HTMLCanvasElement>document.getElementById("canvas")!;
-            prev.classList.add("hidden");
-            startRetroArch(canv, retro_args, function () {
-                let canv = document.getElementById("canvas")!;
-                setInterval(checkChangedSaves, FS_CHECK_INTERVAL);
-                canv.classList.remove("hidden");
-            });
-            return false;
-        });
+  let prev = document.getElementById("webplayer-preview")!;
+  prev.classList.add("loaded");
+  prev.addEventListener(
+    "click",
+    function () {
+      let canv = <HTMLCanvasElement>document.getElementById("canvas")!;
+      prev.classList.add("hidden");
+      startRetroArch(canv, retro_args, function () {
+        let canv = document.getElementById("canvas")!;
+        setInterval(checkChangedSaves, FS_CHECK_INTERVAL);
+        canv.classList.remove("hidden");
+      });
+      return false;
+    });
 }
 
 var renderedSaves = [];
 function checkChangedSaves() {
-    try {
-        var newSaves = FS.readdir("/home/web_user/retroarch/userdata/states");
-        // if any new ones, update lastSaves
-        for (var i = renderedSaves.length; i < newSaves.length; i++) {
-            let save = newSaves[i];
-            console.log(save);
-            renderedSaves.push(save);
-        }
-    } catch (e) {
-        // if (e instanceof ErrnoError) {
-        //     // do nothing
-        // } else {
-        //     throw e;
-        // }
+  try {
+    var newSaves = FS.readdir("/home/web_user/retroarch/userdata/states");
+    // if any new ones, update lastSaves
+    for (var i = renderedSaves.length; i < newSaves.length; i++) {
+      let save = newSaves[i];
+      console.log(save);
+      renderedSaves.push(save);
     }
+  } catch (e) {
+    // if (e instanceof ErrnoError) {
+    //     // do nothing
+    // } else {
+    //     throw e;
+    // }
+  }
 }
 
 // document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
