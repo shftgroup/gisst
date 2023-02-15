1f00e0de617f7cf89f1366615faa97f9534357ca
Joseph C. Osborn <jcoa2018@pomona.edu>
2023-02-15 12:43:37 -0800

    minimal tauri frontend
    - it will hang until retroarch is done, this is temporary
    - next step: watch for save state and video records
    - then: download content from somewhere for this and the web frontend

--- frontend-tauri/src/main.ts
+++ frontend-tauri/src/main.ts
@@ -1,21 +1,45 @@
 import { invoke } from "@tauri-apps/api/tauri";
+import {copyFile} from "@tauri-apps/api/fs";
+import {appCacheDir,join} from "@tauri-apps/api/path";
+import { Command } from '@tauri-apps/api/shell';
 
-let greetInputEl: HTMLInputElement | null;
-let greetMsgEl: HTMLElement | null;
-
-async function greet() {
-  if (greetMsgEl && greetInputEl) {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    greetMsgEl.textContent = await invoke("greet", {
-      name: greetInputEl.value,
-    });
+async function run(core:string, content:string, entryState:bool, movie:bool) {
+  console.assert(!(entryState && movie), "It is invalid to have both an entry state and play back a movie");
+  let content_base = content.substring(0, content.lastIndexOf("."));
+  let cache_dir = await appCacheDir();
+  let content_dir = await join(cache_dir, "content");
+  let retro_args = ["-v", "-c", await join(cache_dir, "retroarch.cfg"), "--appendconfig", await join(content_dir, "retroarch.cfg"), "-L", core];
+  if (entryState) {
+    retro_args.push("-e");
+    retro_args.push("1");
   }
+  if (movie) {
+    retro_args.push("-P");
+    retro_args.push(await join(content_dir,"/movie.bsv"));
+  } else {
+    retro_args.push("-R");
+    retro_args.push(await join(content_dir, "/movie.bsv"));
+  }
+  retro_args.push(await join(content_dir, content));
+  console.log(retro_args);
+  if (entryState) {
+    copyFile(await join(content_dir, "entry_state"), await join(cache_dir, "states", content_base+".state1.entry"));
+  }
+
+  const command = Command.sidecar('binaries/retroarch', retro_args);
+  // TODO: watch proc, send commands, etc
+  const proc = await command.execute();
+  console.log("proc",proc.stdout,proc.stderr);
 }
 
 window.addEventListener("DOMContentLoaded", () => {
-  greetInputEl = document.querySelector("#greet-input");
-  greetMsgEl = document.querySelector("#greet-msg");
   document
-    .querySelector("#greet-button")
-    ?.addEventListener("click", () => greet());
+    .querySelector("#run-cold-button")
+    ?.addEventListener("click", () => run("fceumm", "bfight.nes", false, false));
+  document
+    .querySelector("#run-entry-button")
+    ?.addEventListener("click", () => run("fceumm", "bfight.nes", true, false));
+  document
+    .querySelector("#run-movie-button")
+    ?.addEventListener("click", () => run("fceumm", "bfight.nes", false, true));
 });
