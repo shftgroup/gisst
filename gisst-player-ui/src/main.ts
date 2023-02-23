1cf32c3602838cf3feb5cd483954953282a7efce
Joseph C. Osborn <jcoa2018@pomona.edu>
2023-02-23 12:02:01 -0800

    factor out UI, add UI features to web frontend

--- gisst-player-ui/src/main.ts
+++ gisst-player-ui/src/main.ts
@@ -19,15 +19,16 @@ export class UI {
     const img = new Image();
     const img_data = "data:image/png;base64,"+state_thumbnail_b64_png;
     img.src = img_data;
-    const save_num = parseInt(state_file.match(/state([0-9]+)$/)[1],10);
+    const num_str = (state_file.match(/state([0-9]+)$/)?.[1]) ?? "0";
+    const save_num = parseInt(num_str,10);
     img.addEventListener("click", () => {
       console.log("Load",state_file,save_num);
       this.control["load_state"](save_num);
     });
     this.state_elt.appendChild(img);
   }
-  clearStates() {
-    this.state_elt.childNodes.forEach((elt:HTMLElement) => {
+  clear() {
+    this.state_elt.childNodes.forEach((elt:ChildNode,_key,_par) => {
       elt.remove();
     });
   }
