afce1ac09c3af28d1e4b519eb0c06545784625d5
Eric Kaltman <eric.kaltman@csuci.edu>
2023-04-04 16:08:41 -0700

    Added in demo state listing in states tab

--- gisst-player-ui/vite.config.js
+++ gisst-player-ui/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   build: {
     lib: {
       // Could also be a dictionary or array of multiple entry points
-      entry: resolve(__dirname, 'src/main.ts'),
+      entry: resolve(__dirname, 'src/ts/main.ts'),
       name: 'GisstPlayer',
       // the proper extensions will be added
       fileName: 'gisst-player',
@@ -14,6 +14,12 @@ export default defineConfig({
     outDir:"dist",
   },
   plugins: [dts({skipDiagnostics:false,logDiagnostics:true,insertTypesEntry:true,copyDtsFiles:true,outputDir: ['dist', 'types'],})],
+  resolve: {
+    alias: {
+      '~bootstrap': resolve(__dirname, 'node_modules/bootstrap'),
+    }
+  },
+  root: resolve(__dirname, 'src'),
   rollupOptions: {
     // make sure to externalize deps that shouldn't be bundled
     // into your library
