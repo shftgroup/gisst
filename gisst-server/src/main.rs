9c321561a337b3e96286c52896de4aae6b01c935
Eric Kaltman <eric.kaltman@gmail.com>
2023-04-18 16:26:28 -0700

    Interim commit after adding initial table structs.

--- gisst-server/src/main.rs
+++ gisst-server/src/main.rs
@@ -43,52 +43,37 @@ struct Opt {
 
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), sqlx::Error>{
 
     // Parse CLI commands
     let opt = Opt::parse();
+
+    let app: Router;
 
     // Setup logging and RUST_LOG from args
     if std::env::var("RUST_LOG").is_err() {
         std::env::set_var("RUST_LOG", format!("{},hyper=info,mio=info", opt.log_level))
     }
+
+    // Configure Ip and Port
+    let addr = SocketAddr::from(
+        (IpAddr::from_str(opt.addr.as_str()).unwrap_or(IpAddr::V6(Ipv6Addr::LOCALHOST)),
+         opt.port,
+        ));
+
     // enable console logging
     tracing_subscriber::fmt::init();
 
-    // Run
+    // Run headless
     if opt.no_app {
-
-    } else{
-        tokio::join!(
-            serve(using_serve_dir(), opt.addr, opt.port),
-        );
+        // Nothing implemented right now
+    } else { // Run with web app
+        app = Router::new()
+            .route("/artifact/:artifact_id", get(get_artifact))
+            .nest_service("/", ServeDir::new("../frontend-web/dist"))
     }
 
 
-
-
-    //let pool = PgPoolOptions::new()
-    //    .max_connections(5)
-    //    .connect("postgres://postgres:password@localhost/test")
-    //    .await?;
-}
-
-fn using_serve_dir() -> Router {
-    Router::new()
-        .route("/artifact/:artifact_id", get(get_artifact))
-        .nest_service("/", ServeDir::new("../frontend-web/dist"))
-}
-
-async fn get_artifact(Path(artifact_id):Path<String>) -> Html<String> {
-    let r = render!(INDEX_TEMPLATE, artifact => Artifact { id: artifact_id } );
-    Html(r)
-}
-
-async fn serve(app: Router, ip_addr:String, port:u16){
-    let addr = SocketAddr::from(
-        (IpAddr::from_str(ip_addr.as_str()).unwrap_or(IpAddr::V6(Ipv6Addr::LOCALHOST)),
-        port,
-    ));
     tracing::debug!("listening on {}", ip_addr);
 
     axum::Server::bind(&addr)
@@ -97,21 +82,123 @@ async fn serve(app: Router, ip_addr:String, port:u16){
         .unwrap();
 
     log::info!("listening on http://{}", ip_addr);
+
+
+
+
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect("postgres://eric.kaltman@localhost/testdb")
+        .await?;
+
+    Ok(())
+}
+
+
+async fn get_artifact(Path(artifact_id):Path<String>) -> Html<String> {
+    // Just for testing right now, in future will be a call to the database
+    let core: &'static str = "v86";
+    let r = render!(INDEX_TEMPLATE, content => Content { id: artifact_id, core: core.to_string() } );
+    Html(r)
+}
+
+
+// Database structs, templates, and table generation
+
+#[derive(Debug, Serialize)]
+struct Content {
+    id: i32,
+    uuid: uuid::Uuid,
+    title: String,
+    version: String,
+    path: std::path::Path,
+    filename: String,
+    core: String,
+    core_version: String,
+    image_id: uuid::Uuid,
+    save_id: uuid::Uuid,
+}
+
+const CONTENT_CREATE_TABLE: &'static str = r#"
+CREATE TABLE content (
+    id          serial PRIMARY KEY,
+    uuid        uuid,
+    title       varchar(100),
+    version
+
+)
+"#;
+
+#[derive(Debug, Serialize)]
+struct ContentAddition{
+    id: i32,
+    uuid: uuid::Uuid,
+    content_id: i32,
+    filename: String,
+    path: std::path::Path,
 }
 
 #[derive(Debug, Serialize)]
-struct Artifact {
-    id: String,
+struct Save{
+    id: i32,
+    uuid: uuid::Uuid,
+    short_description: String,
+    description: String,
+    filename: String,
+    path: std::path::Path,
+}
+
+#[derive(Debug, Serialize)]
+struct Replay{
+    id: i32,
+    uuid: uuid::Uuid,
+    content_id: i32,
+    save_id: i32,
+    filename: String,
+    path: std::path::Path,
+}
+
+#[derive(Debug, Serialize)]
+struct State{
+    id: i32,
+    uuid: uuid::Uuid,
+    replay_id: i32,
+    content_id: i32,
+    replay_time_index: u16,
+    is_checkpoint: bool,
+    path: std::path::Path,
+    filename: String,
+    name: String,
+    description: String,
+}
+
+#[derive(Debug, Serialize)]
+struct Image{
+    id: i32,
+    uuid: uuid::Uuid,
+    filename: String,
+    parent_id: i32,
 }
 
 const INDEX_TEMPLATE: &'static str = r#"
 <!doctype html>
 <html lang="en">
 <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>GISST Player</title>
+    <title>Embedulator</title>
+    <script type="module" crossorigin src="/assets/index.js"></script>
+    <link rel="stylesheet" href="/assets/index.css">
+    {% if content.core == "ra" %}
+
+    {% elif content.core == "v86" %}
+    <script src="v86/libv86.js"></script>
+    {% endif %}
 </head>
 
-<body data-artifact-id={{ artifact.id }}>
-Just a test to show the template correctly, {{ artifact.id }}.
+<body data-artifact-id={{ content.id }}>
+    <div id="ui"></div>
 </body>
 </html>
 "#;
