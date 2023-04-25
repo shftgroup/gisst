44617a2824650c5ab5f8661b582338130389df31
Eric Kaltman <eric.kaltman@gmail.com>
2023-04-25 11:58:43 -0700

    Adding sql scripts to fix an SQLX issue

--- gisst-server/migrations/20230420235426_test-inserts.sql
+++ gisst-server/migrations/20230420235426_test-inserts.sql
@@ -2,4 +2,5 @@
 
 INSERT INTO core (core_name, core_version) VALUES ("nes", "0.0.1");
 INSERT INTO platform (core_id, platform_framework) VALUES (1, "retroarch");
+INSERT INTO content (content_title, content_version, content_path, content_filename, platform_id) VALUES ("super mario", "0.1", "hashvalue/human_readable", "test",1);
 
