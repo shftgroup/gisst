3237b7eba35780d6c925282e9155e05743c39027
Eric Kaltman <eric.kaltman@gmail.com>
2023-04-25 11:59:08 -0700

    Sqlx migration modifications

--- gisst-server/migrations/20230420235426_test-inserts.sql
+++
@@ -1,6 +0,0 @@
--- Add migration script here
-
-INSERT INTO core (core_name, core_version) VALUES ("nes", "0.0.1");
-INSERT INTO platform (core_id, platform_framework) VALUES (1, "retroarch");
-INSERT INTO content (content_title, content_version, content_path, content_filename, platform_id) VALUES ("super mario", "0.1", "hashvalue/human_readable", "test",1);
-
