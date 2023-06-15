ba023ecacb17f73871644230b8fe87da036bf937
Joseph C. Osborn <jcoa2018@pomona.edu>
2023-06-15 09:51:38 -0700

    Fix warnings and errors

--- gisst-server/src/lib.rs
+++ gisst-server/src/lib.rs
@@ -1,18 +1,12 @@
 pub mod models;
 pub mod storage;
 use axum::{
+    extract::{multipart::MultipartError, Json},
     http::StatusCode,
-    response::{
-        IntoResponse,
-        Response,
-    },
-    extract::{
-        Json,
-        multipart::MultipartError,
-    },
+    response::{IntoResponse, Response},
 };
-use thiserror::Error;
 use serde_json::json;
+use thiserror::Error;
 
 #[derive(Debug, Error)]
 pub enum GISSTError {
@@ -37,21 +31,27 @@ impl IntoResponse for GISSTError {
         let (status, message) = match self {
             GISSTError::SqlError(_) => (StatusCode::INTERNAL_SERVER_ERROR, "database error"),
             GISSTError::StorageError(_) => (StatusCode::INTERNAL_SERVER_ERROR, "storage error"),
-            GISSTError::RecordCreateError(_) => (StatusCode::INTERNAL_SERVER_ERROR, "record creation error"),
-            GISSTError::RecordUpdateError(_) => (StatusCode::INTERNAL_SERVER_ERROR, "record update error"),
-            GISSTError::PathPrefixError(_) => (StatusCode::INTERNAL_SERVER_ERROR, "file creation error"),
+            GISSTError::RecordCreateError(_) => {
+                (StatusCode::INTERNAL_SERVER_ERROR, "record creation error")
+            }
+            GISSTError::RecordUpdateError(_) => {
+                (StatusCode::INTERNAL_SERVER_ERROR, "record update error")
+            }
+            GISSTError::PathPrefixError(_) => {
+                (StatusCode::INTERNAL_SERVER_ERROR, "file creation error")
+            }
             GISSTError::TemplateError => (StatusCode::INTERNAL_SERVER_ERROR, "template error"),
             GISSTError::Generic => (StatusCode::INTERNAL_SERVER_ERROR, "generic error"),
         };
 
-        let body = Json(json!({"error": message}));
+        let body = Json(json!({ "error": message }));
 
         (status, body).into_response()
     }
 }
 
 impl From<MultipartError> for GISSTError {
-    fn from(error: MultipartError) -> Self {
+    fn from(_error: MultipartError) -> Self {
         GISSTError::Generic
     }
 }
--- gisst-server/src/storage.rs
+++ gisst-server/src/storage.rs
@@ -1,12 +1,8 @@
 use crate::GISSTError;
-use tokio::fs::{
-    create_dir_all,
-    File,
-    remove_file,
-};
+use tokio::fs::{create_dir_all, remove_file, File};
 
-use std::path::{Component, Path, PathBuf};
 use log::info;
+use std::path::{Component, Path, PathBuf};
 use tokio::io::AsyncWriteExt;
 
 use uuid::Uuid;
@@ -18,19 +14,20 @@ pub struct StorageHandler {
 
 pub struct FileInformation {
     pub source_filename: String,
+    pub source_path: String,
     pub dest_filename: String,
     pub dest_path: String,
     pub file_hash: String,
 }
 
 impl StorageHandler {
-    pub fn init(root_storage_path:String, storage_folder_depth:u8) -> StorageHandler {
-
-        if !Path::new(&root_storage_path).is_dir(){
-            std::fs::create_dir_all(&root_storage_path).expect("Unable to create root storage directory");
+    pub fn init(root_storage_path: String, storage_folder_depth: u8) -> StorageHandler {
+        if !Path::new(&root_storage_path).is_dir() {
+            std::fs::create_dir_all(&root_storage_path)
+                .expect("Unable to create root storage directory");
         }
 
-        StorageHandler{
+        StorageHandler {
             root_storage_path,
             folder_depth: storage_folder_depth,
         }
@@ -40,7 +37,7 @@ impl StorageHandler {
         let mut uuid_string = uuid.to_string();
         uuid_string.truncate(length as usize);
         let mut path = PathBuf::new();
-        for val in uuid_string.chars(){
+        for val in uuid_string.chars() {
             path.push(val.to_string());
         }
         path
@@ -49,17 +46,19 @@ impl StorageHandler {
     pub fn get_md5_hash(data: &[u8]) -> String {
         format!("{:x}", md5::compute(data))
     }
-    pub fn get_dest_filename(hash: &str, filename:&str) -> String { format!("{}-{}", hash, filename).to_lowercase() }
+    pub fn get_dest_filename(hash: &str, filename: &str) -> String {
+        format!("{}-{}", hash, filename).to_lowercase()
+    }
     pub fn get_folder_depth_from_path(path: &Path, filename: Option<String>) -> u8 {
         let mut depth = 1;
         let mut path_buf = path.to_path_buf();
-        if filename.is_some() && path_buf.ends_with(&filename.unwrap()){
+        if filename.is_some() && path_buf.ends_with(&filename.unwrap()) {
             path_buf.pop();
         }
-        for component in path_buf.components(){
+        for component in path_buf.components() {
             match component {
                 Component::Normal(_) => depth += 1,
-                _ => ()
+                _ => (),
             }
         }
         depth - 1
@@ -69,7 +68,11 @@ impl StorageHandler {
         s.chars().filter(|c| !c.is_whitespace()).collect()
     }
 
-    pub async fn delete_file_with_uuid(&self, uuid: Uuid, dest_filename: &str) -> tokio::io::Result<()>{
+    pub async fn delete_file_with_uuid(
+        &self,
+        uuid: Uuid,
+        dest_filename: &str,
+    ) -> tokio::io::Result<()> {
         info!("Deleting file with filename: {}", dest_filename);
         let mut path = Path::new(&self.root_storage_path)
             .join(StorageHandler::split_uuid_to_path_buf(uuid, self.folder_depth).as_path());
@@ -78,12 +81,19 @@ impl StorageHandler {
         remove_file(path).await
     }
 
-    pub async fn write_file_to_uuid_folder(&self, uuid: Uuid, filename: &str, file_data: &[u8]) -> Result<FileInformation, GISSTError>{
+    pub async fn write_file_to_uuid_folder(
+        &self,
+        uuid: Uuid,
+        filename: &str,
+        file_data: &[u8],
+    ) -> Result<FileInformation, GISSTError> {
         let mut path = Path::new(&self.root_storage_path)
             .join(StorageHandler::split_uuid_to_path_buf(uuid, self.folder_depth).as_path());
 
         if !path.is_dir() {
-            create_dir_all(path.as_path()).await.expect("Unable to create directory for uuid")
+            create_dir_all(path.as_path())
+                .await
+                .expect("Unable to create directory for uuid")
         }
 
         let hash_string = StorageHandler::get_md5_hash(file_data);
@@ -97,15 +107,19 @@ impl StorageHandler {
 
         path.pop();
 
-        Ok(FileInformation{
+        Ok(FileInformation {
             source_filename: filename.to_string(),
+            // TODO: take in a path and not just a filename, and do away with filename?
+            source_path: filename.to_string(),
             dest_filename: save_filename,
-            dest_path: path.strip_prefix(&self.root_storage_path)?.to_string_lossy().to_string(),
+            dest_path: path
+                .strip_prefix(&self.root_storage_path)?
+                .to_string_lossy()
+                .to_string(),
             file_hash: hash_string.to_string(),
         })
     }
 }
-
 
 #[cfg(test)]
 mod tests {
@@ -113,15 +127,24 @@ mod tests {
     use uuid::uuid;
 
     #[test]
-    fn uuid_path(){
-        let path: PathBuf = StorageHandler::split_uuid_to_path_buf(uuid!("00000000-0000-0000-0000-000000000000"), 4);
+    fn uuid_path() {
+        let path: PathBuf = StorageHandler::split_uuid_to_path_buf(
+            uuid!("00000000-0000-0000-0000-000000000000"),
+            4,
+        );
         assert_eq!(Path::new("0/0/0/0/"), path.as_path());
-        let path2: PathBuf = StorageHandler::split_uuid_to_path_buf(uuid!("00000000-0000-0000-0000-000000000000"), 4);
-        assert_eq!(Path::new("/storage").join(path2), Path::new("/storage/0/0/0/0/"))
+        let path2: PathBuf = StorageHandler::split_uuid_to_path_buf(
+            uuid!("00000000-0000-0000-0000-000000000000"),
+            4,
+        );
+        assert_eq!(
+            Path::new("/storage").join(path2),
+            Path::new("/storage/0/0/0/0/")
+        )
     }
 
     #[test]
-    fn depth_from_dest_path(){
+    fn depth_from_dest_path() {
         let mut path = PathBuf::from("0/0/0/0/somefile.txt");
         path.pop();
         assert_eq!(Path::new("0/0/0/0"), path);
@@ -133,7 +156,13 @@ mod tests {
         let path = Path::new("0/0/0/0/somefile.txt");
         assert_eq!(5, StorageHandler::get_folder_depth_from_path(path, None));
         let path = Path::new("0/0/0/0/somefile.txt");
-        assert_eq!(4, StorageHandler::get_folder_depth_from_path(path, Some("somefile.txt".to_string())));
-        assert_eq!(0, StorageHandler::get_folder_depth_from_path(Path::new(""), None))
+        assert_eq!(
+            4,
+            StorageHandler::get_folder_depth_from_path(path, Some("somefile.txt".to_string()))
+        );
+        assert_eq!(
+            0,
+            StorageHandler::get_folder_depth_from_path(Path::new(""), None)
+        )
     }
 }
