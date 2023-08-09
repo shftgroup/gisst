d5502a4a5a38f2d5ed1ccf09a74caba13718d06f
Joseph C. Osborn <joseph.osborn@pomona.edu>
2023-08-09 10:48:49 -0700

    make rust build script actually build and dist

--- backend/gisst-server/build.rs
+++ backend/gisst-server/build.rs
@@ -2,22 +2,24 @@ use std::process::Command;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     const FRONT_DIR: &str = "../../frontend";
-    if std::fs::metadata(FRONT_DIR)?.is_dir() {
-        dbg!(Command::new("npm").arg("i").current_dir(FRONT_DIR))
-            .output()
-            .expect("Failed to execute npm install");
+    if std::fs::metadata(FRONT_DIR)
+        .map(|md| md.is_dir())
+        .unwrap_or(false)
+    {
+        dbg!(Command::new("npm").arg("i").current_dir(FRONT_DIR)).output()?;
         dbg!(Command::new("npm")
             .args(["run", "build", "--workspaces", "--if-present"])
-            .current_dir(FRONT_DIR));
+            .current_dir(FRONT_DIR))
+        .output()?;
         dbg!(Command::new("npm")
             .args(["run", "dist", "--workspaces", "--if-present"])
             .current_dir(FRONT_DIR))
-        .output()
-        .expect("Failed to execute npm build");
+        .output()?;
     }
     println!("cargo:rerun-if-changed={FRONT_DIR}/ra-util/src");
     println!("cargo:rerun-if-changed={FRONT_DIR}/embedv86/src");
     println!("cargo:rerun-if-changed={FRONT_DIR}/gisst-player-ui/src");
     println!("cargo:rerun-if-changed={FRONT_DIR}/frontend-web/src");
+    println!("cargo:rerun-if-changed={FRONT_DIR}/frontend-web/public");
     Ok(())
 }
