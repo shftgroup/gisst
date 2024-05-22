a6848937e71163d938fc8413213b526f4db17e10
Eric Kaltman <eric.kaltman@gmail.com>
2024-05-22 13:04:32 -0400

    Fixing clippy issue

--- backend/gisst-server/src/error.rs
+++ backend/gisst-server/src/error.rs
@@ -82,7 +82,7 @@ impl IntoResponse for GISSTError {
             embed("https://gisst.pomona.edu/data/62f78345-b4b0-458d-a6ea-5c08724a6415?state=e32b9c0f-f56e-4a84-b2e6-e4996a82e35a", document.getElementById("embedExample"));
         </script>
         "#).unwrap();
-        error!("{}", self.source().clone().unwrap().to_string());
+        error!("{}", self.source().unwrap().to_string());
         let error_template = env.get_template("error.html").unwrap();
         let (status, message) = match self {
             GISSTError::SqlError(_) => (StatusCode::INTERNAL_SERVER_ERROR, "database error"),
