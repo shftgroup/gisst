d42b02bf3898af21729c0a1e690cfb03b6dca2dd
Joseph C. Osborn <jcoa2018@pomona.edu>
2025-02-23 09:21:13 -0800

    fix silly hard coded path

--- frontend/ra-util/eslint.config.mjs
+++ frontend/ra-util/eslint.config.mjs
@@ -26,8 +26,7 @@ export default [
             sourceType: "script",
 
             parserOptions: {
-                tsconfigRootDir: "/home/jcoa2018/Projects/embedulator/frontend/ra-util",
-                project: true,
+                project: true
             },
         },
 
