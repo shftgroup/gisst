5972d0e164cee75495554f0469288d2fe0d27e45
Joe Osborn <jcoa2018@pomona.edu>
2025-03-12 20:30:23 -0700

    Reduce duplication in dist update
    Also keep a few more assets in the bundle

--- dist-update.sh
+++ dist-update.sh
@@ -43,8 +43,8 @@ unzip overlays.zip -d overlays
 rm overlays.zip
 unzip info.zip -d info
 rm info.zip
-rm -rf overlays/{borders,ctr,effects,ipad,keyboards,misc,wii}
-rm -rf overlays/gamepads/{720-med,arcade,arcade-anim,arcade-minimal,cdi_anim_portrait,dual-shock,example,flat,flip_phone,gba-anim_landscape,gba-grey,gb_anim_portrait,genesis,lite,n64,neo-retropad,old,psx,rgpad,scummvm}
+rm -rf overlays/{borders,ctr,effects,ipad,misc,wii}
+rm -rf overlays/gamepads/{720-med,arcade,arcade-anim,arcade-minimal,cdi_anim_portrait,example,flat,flip_phone,gba-anim_landscape,gba-grey,gb_anim_portrait,genesis,lite,n64,old,rgpad,scummvm}
 rm -f assets/ozone/png/icons/*\ -\ *
 rm -f assets/sounds/*.wav
 rm -f assets/pkg/chinese-*
@@ -77,15 +77,17 @@ fi
 if [ ${BUILD_CORES:-1} -eq 1 ]; then
 pushd ra-build
 
-cd ra
+pushd ra
 rm -rf obj-emscripten
-git pull
-cd ..
+git pull || echo "RA directory dirty or pull failed for other reason"
+popd
 
-# todo: fix and put back hatari
-for f in {fceumm,snes9x,pcsx_rearmed,vba_next,gambatte,stella2014}; do
+for f in *; do
+    if [ $f = "ra" ]; then 
+      continue
+    fi
     pushd $f
-    # git pull
+    git pull || echo "${f} pull failed"
     if [ -f Makefile.libretro ]
     then
         # emmake make -f Makefile.libretro platform=emscripten clean
