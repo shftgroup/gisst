1cdd45b76e4509efe18685181e3b587b828ef2b7
Joseph C. Osborn <jcoa2018@pomona.edu>
2025-04-30 12:35:49 -0700

    use JoeOsborn fork of retroarch again for add-save-load-sram-commands branch

--- dist-update.sh
+++ dist-update.sh
@@ -48,7 +48,7 @@ mkdir -p cores
 emsdk install 4.0.7
 emsdk activate 4.0.7
 git clone --depth 1 -b tick-event https://github.com/JoeOsborn/v86 v86 || echo "already have v86"
-git clone --depth 1 https://github.com/libretro/retroarch ra || echo "already have RA"
+git clone --depth 1 https://github.com/JoeOsborn/retroarch ra || echo "already have RA"
 git clone --depth 1 https://github.com/libretro/libretro-fceumm fceumm || echo "already have fceumm"
 git clone --depth 1 https://github.com/libretro/snes9x snes9x || echo "already have snes9x"
 git clone --depth 1 -b emscripten-build https://github.com/JoeOsborn/hatari hatari || echo "already have hatari"
@@ -65,6 +65,7 @@ if [ ${BUILD_CORES:-1} -eq 1 ]; then
 pushd ra-build
 
 pushd ra
+git checkout add-save-load-sram-commands
 rm -rf obj-emscripten
 git pull || echo "RA directory dirty or pull failed for other reason"
 popd
