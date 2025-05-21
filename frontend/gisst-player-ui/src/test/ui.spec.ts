24e99805ce60f6f80c0e54bac75b8c25459f9039
Stavros Zografos <stavzog@gmail.com>
2025-05-21 12:21:18 +0300

    fixed semicolon inconsitencies in test files

--- frontend/gisst-player-ui/src/test/ui.spec.ts
+++ frontend/gisst-player-ui/src/test/ui.spec.ts
@@ -14,8 +14,8 @@ test('Recoding Performances', async ({ page }) => {
   const gisstStatesList = page.locator('#gisst-replays-list');
   const initialCount = await gisstStatesList.locator('li.replay-list-item').count();
 
-  await page.getByRole('button', {name:'Start Performance'}).click()
-  await page.getByRole('button', {name:'Finish Performance'}).click()
+  await page.getByRole('button', { name: 'Start Performance' }).click();
+  await page.getByRole('button', { name: 'Finish Performance' }).click();
 
 
   // Expects the Save State button to have added another state to the list
@@ -28,7 +28,7 @@ test('Save State button', async ({ page }) => {
   const gisstStatesList = page.locator('#gisst-states-list');
   const initialCount = await gisstStatesList.locator('div[id*=state]').count();
 
-  await page.getByRole('button', {name:'Save State'}).click()
+  await page.getByRole('button', { name: 'Save State' }).click();
 
   // Expects the Save State button to have added another state to the list
   await expect(gisstStatesList.locator('div[id*=state]')).toHaveCount(initialCount+1);
