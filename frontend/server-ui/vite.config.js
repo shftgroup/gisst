40e460392a14de7d968accff24e6065d016c64db
Joe Osborn <jcoa2018@pomona.edu>
2025-05-23 17:03:54 +0000

    Trim down server-ui vite.config.js
    Maybe some of this will be added back in later but for now it’s just noise

--- frontend/server-ui/vite.config.js
+++ frontend/server-ui/vite.config.js
@@ -1,23 +1,21 @@
 import { resolve, extname } from 'path'
 import checker from 'vite-plugin-checker';
 import sourcemaps from 'rollup-plugin-sourcemaps';
-import mkcert from 'vite-plugin-mkcert';
 import fs from 'node:fs';
 import dts from 'vite-plugin-dts'
 
 export default {
   base: "./",
   define: {
-  'process.env': {'NODE_ENV': 'production'}
+    'process.env': {'NODE_ENV': 'production'}
   },
   plugins: [
-    mkcert({savePath: "../../test-cert/"}),
     checker({
       // e.g. use TypeScript check
       typescript: true,
     }),
     dts({skipDiagnostics:false,logDiagnostics:true,insertTypesEntry:true,copyDtsFiles:true,outputDir: ['dist', 'types'],}),
-      sourcemaps()
+    sourcemaps()
   ],
   build: {
     lib: {
@@ -26,23 +24,5 @@ export default {
       fileName: 'ui',
     },
     sourcemap: true,
-  },
-  server: {
-    headers:{
-      "Cross-Origin-Embedder-Policy":"require-corp",
-      "Cross-Origin-Resource-Policy":"cross-origin",
-      "Cross-Origin-Opener-Policy":"same-origin"
-    },
-    proxy: {
-      "/storage": {
-        changeOrigin: false,
-        secure: false,
-        target: {
-          protocol: 'https:',
-          host: 'localhost',
-          port: 3000,
-        },
-      }
-    }
   }
 }
