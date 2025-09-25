a2501957d52e0746272d29b7e73aa8e628ff99b4
Joseph C. Osborn <joseph.osborn@pomona.edu>
2025-09-25 09:17:57 -0700

    more test coverage

--- frontend/embedv86/test/blockindex.test.ts
+++ frontend/embedv86/test/blockindex.test.ts
@@ -1,11 +1,14 @@
-import { assert, expect, test } from 'vitest'
+import { expect, test } from 'vitest'
 import { BlockIndex } from '../src/blockindex'
 
-
-test("insert", async () => {
+test("init", async () => {
   const idx = await BlockIndex.create(8);
   expect(idx.length()).toBe(1);
   expect(idx.get(0)).toEqual(Uint8Array.from([0,0,0,0,0,0,0,0]));
+});
+
+test("insert", async () => {
+  const idx = await BlockIndex.create(8);
   const ins1 = await idx.insert(Uint8Array.from([0,0,0,0,0,0,0,1]),1);
   expect(ins1.is_new).toBe(true);
   expect(ins1.index).toBe(1);
@@ -18,8 +21,6 @@ test("insert", async () => {
 
 test("insert_exact", async () => {
   const idx = await BlockIndex.create(8);
-  expect(idx.length()).toBe(1);
-  expect(idx.get(0)).toEqual(Uint8Array.from([0,0,0,0,0,0,0,0]));
   const ins1 = await idx.insert_exact(Uint8Array.from([0,0,0,0,0,0,0,1]),1,1);
   expect(ins1).toBe(true);
   expect(idx.length()).toBe(2);
@@ -29,4 +30,20 @@ test("insert_exact", async () => {
   const ins1b = await idx.insert_exact(Uint8Array.from([0,0,0,0,0,0,0,2]),1,1);
   expect(ins1b).toBe(false);
   expect(idx.length()).toBe(2);
+});
+
+
+test("remove_after", async () => {
+  const idx = await BlockIndex.create(8);
+  await idx.insert(Uint8Array.from([0,0,0,0,0,0,1,1]),1);
+  await idx.insert(Uint8Array.from([0,0,0,0,0,0,2,1]),2);
+  await idx.insert(Uint8Array.from([0,0,0,0,0,0,2,2]),2);
+  await idx.insert(Uint8Array.from([0,0,0,0,0,0,3,1]),3);
+  expect(idx.length()).toBe(5);
+  idx.remove_after(3);
+  expect(idx.length()).toBe(5);
+  idx.remove_after(2);
+  expect(idx.length()).toBe(4);
+  idx.remove_after(0);
+  expect(idx.length()).toBe(1);
 });
