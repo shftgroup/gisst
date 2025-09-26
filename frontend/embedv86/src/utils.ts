f5b54d2d5137aaed89990dd855531536d073b4e6
Joseph C. Osborn <jcoa2018@pomona.edu>
2025-09-26 11:15:18 -0700

    fix byte size bug

--- frontend/embedv86/src/utils.ts
+++ frontend/embedv86/src/utils.ts
@@ -1,6 +1,6 @@
 export function memcmp(a:Uint8Array, b:Uint8Array) : boolean {
-  const a32 = new Uint32Array(a.buffer, a.byteOffset, a.byteLength/8);
-  const b32 = new Uint32Array(b.buffer, b.byteOffset, b.byteLength/8);
+  const a32 = new Uint32Array(a.buffer, a.byteOffset, a.byteLength/4);
+  const b32 = new Uint32Array(b.buffer, b.byteOffset, b.byteLength/4);
   const sz = a32.length;
   if (sz != b32.length) {
     return false;
