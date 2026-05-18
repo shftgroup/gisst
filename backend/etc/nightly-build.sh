acec8f76cbcc43ea221d0f80f88f0a4aae36b0b0
Joseph C. Osborn <joseph.osborn@pomona.edu>
2026-05-18 09:32:03 -0700

    build and upload cores nightly (#27)
    This shows how to extend nightly-build.sh with the ability to upload core bundles.  This iteration uploads potentially sensitive material to the shftgroup org, but since the group is private the package is too.  In the future we may want a private group and a public group and in that case we’d upload different packages to those two groups.
    
    Reviewed-on: https://cafe.cs.pomona.edu/shftgroup/gisst/pulls/27

--- backend/etc/nightly-build.sh
+++ backend/etc/nightly-build.sh
@@ -29,3 +29,19 @@ docker build --platform linux/arm64,linux/amd64 \
 	-f Dockerfile.dist \
 	-t cafe.cs.pomona.edu/shftgroup/gisst-dist:latest\
 	--push .
+echo "build cores"
+OLD_VSN=$(cat last_build || echo "")
+NEW_VSN=$(cat manifest.json Dockerfile.dist dist-update-docker.sh | sha256sum | cut -d' ' -f1)
+if [ "$OLD_VSN" != "$NEW_VSN" ]; then
+  rsync -a ~/Projects/gisst/{psxbios,v86bios} .
+  if [ -f build/assets_minimal.tgz ]; then
+    ASSETS=0
+  else
+    ASSETS=1
+  fi 
+  docker run --rm -v $(pwd)/build:/out -v $(pwd)/manifest.json:/manifest.json -v $(pwd)/v86bios:/files/v86bios -v $(pwd)/psxbios:/files/psxbios -v gisst-build:/gisst-build -e GET_ASSETS=$ASSETS cafe.cs.pomona.edu/shftgroup/gisst-dist:latest
+  tar czvf cores.tgz build
+  curl -u "jcoa2018:$JOB_TOKEN" --anyauth --request DELETE "https://cafe.cs.pomona.edu/api/packages/shftgroup/generic/cores/latest/cores.tgz" || echo "no existing package to delete"
+  curl -u "jcoa2018:$JOB_TOKEN" --anyauth --upload-file cores.tgz "https://cafe.cs.pomona.edu/api/packages/shftgroup/generic/cores/latest/cores.tgz"
+  echo $NEW_VSN > last_build
+fi
