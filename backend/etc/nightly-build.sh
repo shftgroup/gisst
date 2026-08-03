356b244f43132f12d2332f9ed349cd9e516bb496
Joseph C. Osborn <joseph.osborn@pomona.edu>
2026-08-03 13:16:49 -0700

    Post-0.2.4 fallout, lockfiles, core updates, DB schema updates to fix sqlx bugs, nightly build tweaks

--- backend/etc/nightly-build.sh
+++ backend/etc/nightly-build.sh
@@ -3,17 +3,17 @@
 set -e
 set -o pipefail
 
-if [ ${TESTING:-0} -eq 0 ]; then
 WORKDIR=${WORKDIR:-/tmp/gisst}
 if [ -e $WORKDIR ]; then
     pushd $WORKDIR
-    git pull origin main
+    if [ ${NOPULL:-0} -eq 0 ]; then
+        git pull origin main
+    fi
 else
     git clone ssh://forgejo@cafe.cs.pomona.edu:47147/shftgroup/gisst $WORKDIR
     pushd $WORKDIR
 fi
 cat $JOB_TOKEN | docker login cafe.cs.pomona.edu -u jcoa2018 --password-stdin
-fi
 echo "build ci"
 docker build --platform linux/arm64,linux/amd64 \
 	-f Dockerfile.ci \
