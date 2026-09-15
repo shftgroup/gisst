50ac020260d3c5edadbe7fbe245d04eb107b32ff
Jacky161 <jacky.161@outlook.com>
2026-09-14 23:40:56 -0600

    test: create savefile button, tweak record performance test wording

--- frontend/gisst-player-ui/src/test/ui.spec.ts
+++ frontend/gisst-player-ui/src/test/ui.spec.ts
@@ -8,19 +8,19 @@ test("has title", async ({ page }) => {
   await expect(page).toHaveTitle("Vite + TS");
 });
 
-test("Recoding Performances", async ({ page }) => {
+test("Recording Performances", async ({ page }) => {
   await page.goto("/");
 
-  const gisstStatesList = page.locator("#gisst-replays-list");
-  const initialCount = await gisstStatesList
+  const gisstReplayList = page.locator("#gisst-replays-list");
+  const initialCount = await gisstReplayList
     .locator("li.replay-list-item")
     .count();
 
   await page.getByRole("button", { name: "Start Performance" }).click();
   await page.getByRole("button", { name: "Finish Performance" }).click();
 
-  // Expects the Save State button to have added another state to the list
-  await expect(gisstStatesList.locator("li.replay-list-item")).toHaveCount(
+  // Expects recording replay to have added another replay to the list
+  await expect(gisstReplayList.locator("li.replay-list-item")).toHaveCount(
     initialCount + 1,
   );
 });
@@ -35,6 +35,20 @@ test("Save State button", async ({ page }) => {
 
   // Expects the Save State button to have added another state to the list
   await expect(gisstStatesList.locator("div[id*=state]")).toHaveCount(
+    initialCount + 1,
+  );
+});
+
+test("Create Savefile button", async ({ page }) => {
+  await page.goto("/");
+
+  const gisstSaveList = page.locator("#gisst-saves");
+  const initialCount = await gisstSaveList.locator("div.card-list-object").count();
+
+  await page.getByRole("button", { name: "Create Savefile" }).click();
+
+  // Expects the Create Savefile button to have added another save to the list
+  await expect(gisstSaveList.locator("div.card-list-object")).toHaveCount(
     initialCount + 1,
   );
 });
